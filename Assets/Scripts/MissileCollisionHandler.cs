cfbd4375259ddddef48e61c30bc90bf70c8598c1
José Baldwin <83647279+josebaldwin@users.noreply.github.com>
2023-12-27 09:30:12 -0300

    Misiles
    debe repararse, no se destruyen al impacto

--- Assets/Scripts/MissileCollisionHandler.cs
+++ Assets/Scripts/MissileCollisionHandler.cs
@@ -9,9 +9,27 @@ public class MissileCollisionHandler : MonoBehaviour
         // Check if the collider belongs to an enemy
         if (other.CompareTag("Enemy"))
         {
-            // Destroy the missile and the enemy after a short delay
+            // Destroy the missile immediately upon impact
             Destroy(gameObject);
-            Destroy(other.gameObject, 0.2f); // Adjust the delay as needed
+
+            // Access the Rigidbody of the enemy and set it as not kinematic (if using Rigidbody)
+            Rigidbody enemyRigidbody = other.GetComponent<Rigidbody>();
+            if (enemyRigidbody != null)
+            {
+                enemyRigidbody.isKinematic = false;
+            }
+
+            // Start a coroutine to destroy the enemy after a delay (e.g., 0.2 seconds)
+            StartCoroutine(DestroyEnemyAfterDelay(other.gameObject, 0.2f)); // Adjust the delay as needed
         }
+    }
+
+    IEnumerator DestroyEnemyAfterDelay(GameObject enemy, float delay)
+    {
+        // Wait for the specified delay
+        yield return new WaitForSeconds(delay);
+
+        // Destroy the enemy object
+        Destroy(enemy);
     }
 }
