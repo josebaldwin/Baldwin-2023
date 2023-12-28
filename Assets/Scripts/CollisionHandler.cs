0b1b8f4f281a87f4dcfee7671ed7730a25f94850
José Baldwin <83647279+josebaldwin@users.noreply.github.com>
2023-12-28 10:18:27 -0300

    Misil arreglado
    El misil ahora se destruye siempre upon impact

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
@@ -12,20 +11,22 @@ public class CollisionHandler : MonoBehaviour
         // Check if the collision involves a Missile and hasn't been hit yet
         if (collision.gameObject.CompareTag("Missile"))
         {
+            // Destroy the missile immediately upon impact
+            Destroy(collision.gameObject);
+
             if (resistanceCounter < maxResistance)
             {
                 // Enemy is resistant, do not get affected
                 resistanceCounter++;
 
-                
-            }
-            else
-            {
-                // Destroy the missile immediately upon impact
-                Destroy(collision.gameObject);
+                // Optionally, you can play a sound or add other effects for resistant hits
+                Debug.Log("Missile collided with a resistant enemy!");
 
-                // Start a coroutine to destroy the object after a delay (e.g., 2 seconds)
-                StartCoroutine(DestroyAfterDelay(2f)); // Adjust the delay as needed
+                if (resistanceCounter >= maxResistance)
+                {
+                    // Start a coroutine to destroy the object after a delay (e.g., 2 seconds)
+                    StartCoroutine(DestroyAfterDelay(2f)); // Adjust the delay as needed
+                }
             }
         }
     }
@@ -35,7 +36,7 @@ public class CollisionHandler : MonoBehaviour
         // Wait for the specified delay
         yield return new WaitForSeconds(delay);
 
-        // Destroy the object this script is attached to (enemy or missile)
+        // Destroy the object this script is attached to (enemy)
         Destroy(gameObject);
     }
 }
